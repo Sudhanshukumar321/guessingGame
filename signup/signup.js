7bca126222fa1cade496ed27b7b54b531a2d4015
sudhanshu kumar <104273241+Sudhanshukumar321@users.noreply.github.com>
2023-05-17 12:42:45 +0530

    signup folder added

---
+++ signup/signup.js
@@ -0,0 +1,9 @@
+// let form = document.querySelector('.signup_hidden');
+// let log = document.querySelector(".sign_up");
+
+// const signup_popup = () =>{
+//     form.classList.toggle('signup_hidden');
+// }
+
+// log.addEventListener('click',signup_popup);
+
