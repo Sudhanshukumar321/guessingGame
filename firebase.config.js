1395a425290a145d97e595730f05b425862c7195
sudhanshu kumar <104273241+Sudhanshukumar321@users.noreply.github.com>
2023-05-17 12:49:15 +0530

    firebaseconfig.js folder added

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA5I5bzQhv84sLhj3jqpOKTkl0BzU-rcmk",
+  authDomain: "wordscramblinggame-f28b8.firebaseapp.com",
+  projectId: "wordscramblinggame-f28b8",
+  storageBucket: "wordscramblinggame-f28b8.appspot.com",
+  messagingSenderId: "640275556679",
+  appId: "1:640275556679:web:a3ed258297e30dc54d8462"
+};
